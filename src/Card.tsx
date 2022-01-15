75cd112df21ef65195a36e1a33620ab1735b7b96
Wil Moore III <wil.moore@wilmoore.com>
2022-01-15 03:17:00 -0500

    Gunna Beeches

--- src/Card.tsx
+++ src/Card.tsx
@@ -8,8 +8,9 @@ import { UnicodeLogo } from './UnicodeLogo';
 
 const anthem = {
   Bezerk: '//content.beatstars.com/users/prod/224460/a4t704kbe.mp3',
-  TheseDays: '//cloudup.com/cAtzezeD_tk+',
   EightiesFeel: '//main.v2.beatstars.com/stream?id=5870232&return=audio&timestamp=1640578239',
+  Gunna: 'https://s3.amazonaws.com/beatstarsdata/b.user.data/k/kosmusic-158021/stream/2716147_custom.mp3',
+  TheseDays: '//cloudup.com/cAtzezeD_tk+',
   Toxic: 'https://main.v2.beatstars.com/stream?id=9755794&return=audio&timestamp=1642231148',
 };
 
@@ -35,7 +36,7 @@ export default function Card() {
 
   return (
     <CenteredSingleColumn>
-      <UnicodeLogo code={code} isPlaying={isPlaying} anthem={anthem.Toxic} />
+      <UnicodeLogo code={code} isPlaying={isPlaying} anthem={anthem.Gunna} />
       <NameTag name={name} />
       <Menu sideBarHandler={sideBarHandler} pausePlay={pausePlay} isPlaying={isPlaying} />
       <SideBar isOpen={isOpen} setIsOpen={setIsOpen} page={page} />
