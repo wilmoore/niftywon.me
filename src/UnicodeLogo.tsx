46faa8eb61369cea8daf724b2f383905176cf0f6
Wil Moore III <wil.moore@wilmoore.com>
2022-01-19 22:47:00 -0500

    NOSPIN

--- src/UnicodeLogo.tsx
+++ src/UnicodeLogo.tsx
@@ -3,38 +3,39 @@ import { Link } from './Link'
 import { useState } from 'react'
 import { WonAudioPlayer } from './WonAudioPlayer'
 
-const UnicodeLogoContainer = styled.div`
+const ViewBox = styled.div`
   position: relative;
   width: 350px;
   height: 350px;
 `
 
-interface UnicodeLogoProps {
+interface Props {
   code: string
   symbol: string
 }
 
-export const UnicodeLogo = ({ code, symbol }: UnicodeLogoProps) => {
-  const [imageClassNames, setImageClassNames] = useState('')
+const NOSPIN = ''
+
+export const UnicodeLogo = ({ code, symbol }: Props) => {
+  const [imageClassNames, setImageClassNames] = useState(NOSPIN)
   const [isPlaying, setIsPlaying] = useState(false)
 
   const href = `https://unicode-table.com/en/${code}/`
   const logo = `https://raw.githubusercontent.com/wilmoore/wonlogo.svg/main/index.svg?${Date.now()}`
-  const isSpinning = (enable: boolean) => enable ? 'spin' : ''
 
   const onPlayPause = (event: any) => {
     const isPlaying = (event.type === 'play')
     setIsPlaying(isPlaying)
-    setImageClassNames(isSpinning(isPlaying))
+    setImageClassNames(isPlaying ? 'spin' : NOSPIN)
   }
 
   return (
-    <UnicodeLogoContainer>
+    <ViewBox>
       <WonAudioPlayer isPlaying={isPlaying} onPlayPause={onPlayPause} />
       <Link href={href}>
         <img id='unicode-logo' className={imageClassNames} src={logo} alt={symbol} />
       </Link>
-    </UnicodeLogoContainer>
+    </ViewBox>
   )
 }
 
