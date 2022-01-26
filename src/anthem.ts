3d5342f9d97c1d38c1c12b7c944d78d2b6641ffb
Wil Moore III <wil.moore@wilmoore.com>
2022-01-26 01:56:37 -0500

    exclusive

--- src/anthem.ts
+++ src/anthem.ts
@@ -1,8 +1,3 @@
 export const anthem = {
-  CHANEL: 'https://main.v2.beatstars.com/stream?id=4852713&return=audio&timestamp=1642651361',
-  Bezerk: '//content.beatstars.com/users/prod/224460/a4t704kbe.mp3',
-  EightiesFeel: '//main.v2.beatstars.com/stream?id=5870232&return=audio&timestamp=1640578239',
-  Gunna: 'https://s3.amazonaws.com/beatstarsdata/b.user.data/k/kosmusic-158021/stream/2716147_custom.mp3',
-  TheseDays: '//cloudup.com/cAtzezeD_tk+',
-  Toxic: 'https://main.v2.beatstars.com/stream?id=9755794&return=audio&timestamp=1642231148'
+  CHANEL: 'https://cldup.com/b_Vc3T6GNl.wav',
 }
