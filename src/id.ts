0621f104b1b1b3eb99ace63abfc57172139f2a67
Wil Moore III <wil.moore@wilmoore.com>
2022-01-30 00:07:16 -0500

    2022-01-30T05:07:16Z

--- src/id.ts
+++ src/id.ts
@@ -1,4 +1,13 @@
+export const id = '₩'
 export const code = '20A9'
 export const escape = parseInt(code, 16)
 export const symbol = String.fromCodePoint(escape)
 export const name = `Wil (${symbol}) Moore III`
+export const photo = 'https://cldup.com/SU6cLiLWM0.jpg'
+export const bio = '⩭⪼ I build _nifty_ web(3), api & äppz. I get your MVP or Product Demo shipped, you get funded, I walk away with ₩%! Book a Session 🤙🏾💜🌈🦄.'
+export const logo = 'https://raw.githubusercontent.com/wilmoore/wonlogo.svg/main/index.png'
+export const website = 'https://wilmoore.com'
+export const verse = 'Proverbs 3:1-26 NLT'
+export const company = 'Polyglot, LLC'
+export const title = 'CTO'
+export const role = 'Serial Tech Co-Founder'
