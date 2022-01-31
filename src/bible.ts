ae4a00fa9e1fed4d09e0e2edb8a141c53257c7a5
Wil Moore III <wil.moore@wilmoore.com>
2022-01-31 08:28:33 -0500

    2022-01-31T13:28:33Z

--- src/bible.ts
+++ src/bible.ts
@@ -6,6 +6,7 @@ import { lookup } from './translations'
 import { ref } from './id'
 
 const tokens = tokenizer(ref)
+
 const book = (tokens.find(( token ) => { return token.type === 'BOOK' }))?.value || 'Genesis'
 const passage = (tokens.find(( token ) => { return token.type === 'PASSAGE' }))?.value || '1:1'
 const translation = (tokens.find(( token ) => { return token.type === 'TRANSLATION' }))?.value || 'NLT'
