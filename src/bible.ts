b21c9146fefa45c0fd6e9a2d9a9e3c5e119416c3
Wil Moore III <wil.moore@wilmoore.com>
2022-02-02 20:36:56 -0500

    2022-02-03T01:36:56Z

--- src/bible.ts
+++ src/bible.ts
@@ -2,11 +2,13 @@ import { tokenizer } from './bible-reference-tokenizer'
 import { bookup } from './books'
 import { lookup } from './translations'
 import { ref } from './id'
+
 export { ref } from './id'
 
 const tokens = tokenizer(ref)
-const book = (tokens.find(( token ) => { return token.type === 'BOOK' }))?.value || 'Genesis'
-const passage = (tokens.find(( token ) => { return token.type === 'PASSAGE' }))?.value || '1:1'
-const translation = (tokens.find(( token ) => { return token.type === 'TRANSLATION' }))?.value || 'NLT'
+
+const book = (tokens.find(( token ) => token.type === 'BOOK' ))?.value || 'Genesis'
+const passage = (tokens.find(( token ) => token.type === 'PASSAGE' ))?.value || '1:1'
+const translation = (tokens.find(( token ) => token.type === 'TRANSLATION' ))?.value || 'NLT'
 
 export const href = `//bible.com/bible/${lookup(translation)}/${bookup(book)}.${passage.replace(':', '.')}.${translation}`
--- src/translations.js
+++ src/translations.js
@@ -1,12 +1,14 @@
 export const index: { [key: string]: string } = () => ({
-  'AMP'   : 1588,
-  'NBG'   : 328,
-  'NKJV'  : 114,
-  'NLT'   : 116,
-  'NIV'   : 111,
-  'TB'    : 306,
+  'AMP'  : 1588,
+  'NBG'  : 328,
+  'NKJV' : 114,
+  'NLT'  : 116,
+  'NIV'  : 111,
+  'TB'   : 306,
 })
 
 export const lookup = (key) => index()[key]
 
 export const translations = () => Object.keys(index())
+
+export default translations
