65d9932d2dd4bea84cb5fec5d7c438792c7b4044
Wil Moore III <wil.moore@wilmoore.com>
2022-02-07 16:11:02 -0500

    extract bible url path

--- src/bible-url.ts
+++ src/bible-url.ts
@@ -5,6 +5,7 @@ import { lookup } from './bible-reference-tokenizer/translations'
 export const url = (ref: string) => {
   const tokens = tokenizer(ref)
   const { book, passage, translation } = parser(tokens)
+  const path = `${lookup(translation)}/${bookup(book)}.${passage.replace(':', '.')}.${translation}`
 
-  return `//bible.com/bible/${lookup(translation)}/${bookup(book)}.${passage.replace(':', '.')}.${translation}`
+  return `//bible.com/bible/${path}`
 }
