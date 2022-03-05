f0ec72fd9a9926a17f9af4a868141f586e1a50ac
Wil Moore III <wil.moore@wilmoore.com>
2022-03-04 19:07:44 -0500

    Isaiah 2:4

--- src/id.ts
+++ src/id.ts
@@ -12,15 +12,18 @@ export const phone = '+1.303.944.4251'
 export const photo = '//cldup.com/SU6cLiLWM0.jpg'
 export const ref = [
   'Isaiah 2:4',
-  'Genesis 2:1-3 NLT',
-  '1 Peter 4:10 NLT',
-  'Ephesians 5:19',
-  'Acts 2:42',
-  'Hebrews 10:25',
-  '1 Peter 2:13–17',
-  'Romans 13:1–7',
-  'Acts 5:29',
 ]
+// export const ref = [
+//   'Isaiah 2:4',
+//   'Genesis 2:1-3 NLT',
+//   '1 Peter 4:10 NLT',
+//   'Ephesians 5:19',
+//   'Acts 2:42',
+//   'Hebrews 10:25',
+//   '1 Peter 2:13–17',
+//   'Romans 13:1–7',
+//   'Acts 5:29',
+// ]
 export const role = 'CTO'
 export const symbol = String.fromCodePoint(escape)
 export const title = 'Tech Mentor'
