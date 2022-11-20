0ad08fa6379cce7ccaa5c0cbfee2bfb770712f20
Wil Moore III <51953+wilmoore@users.noreply.github.com>
2022-11-20 18:54:50 -0500

    https://user-images.githubusercontent.com

--- src/WonAudioPlayer/anthem.ts
+++ src/WonAudioPlayer/anthem.ts
@@ -2,5 +2,7 @@ export const anthem = {
   CHANEL: 'https://cldup.com/b_Vc3T6GNl.wav',
   // ffmpeg -i Evil\ Needle\ -\ Light\ It\ Up\ \(ft.\ Aleisha\ Lee\)-HDQjnPyF3HY.webm -vn evil-needle-light-it-up.wav
   // ffmpeg -i evil-needle-light-it-up.wav -vn -ar 44100 -ac 2 -b:a 192k evil-needle-light-it-up.mp3
-  LIGHT_IT_UP: 'https://user-images.githubusercontent.com/51953/202933096-d04fb67c-c185-44e3-bd69-819737184241.mov',
+  // https://github.blog/changelog/2020-12-16-video-upload-public-beta/
+  // https://stackoverflow.com/a/61540780
+  LIGHT_IT_UP: '//user-images.githubusercontent.com/51953/202933096-d04fb67c-c185-44e3-bd69-819737184241.mov',
 }
